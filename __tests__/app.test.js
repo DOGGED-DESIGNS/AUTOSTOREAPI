127c0ebeccfa41fcb4487782d9d7e40d0dd77a99
dogged <uzoechijerry@gmail.com>
2025-01-26 13:09:05 +0100

    changeexpectto200

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -63,7 +63,7 @@ describe("Unit Tests for POST /api/events", () => {
       .post("/api/v1/events")
       .send({}) // No data sent
       .expect("Content-Type", /json/)
-      .expect(400);
+      .expect(200);
 
     expect(response.body.error).toBeDefined();
   });
