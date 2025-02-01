b64b0ae5d7168fa6024e7cab6491d746e10587b3
dogged <uzoechijerry@gmail.com>
2025-02-01 07:14:23 +0100

    addedapptestfile

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,18 @@
 module.exports = {
+  // use ts-jest preset for testing Typescript files with jest
   preset: "ts-jest",
   //set the test environment to Node.js
   testEnvironment: "node",
 
   //Define the rood directory for test and modules
-  roots: ["<rootDir>/tests"],
+  roots: ["<rootDir>/__tests__"],
 
   // the ts-jest to transform typspti file
   transform: {
     "^.+\\.ts$": "ts-jest",
   },
   //Regular expression to fid the test files
+  testRegex: ".*\\.(test|spec)\\.(js|ts|jsx|tsx)$",
 
   moduleFileExtensions: ["ts", "js"],
 };
